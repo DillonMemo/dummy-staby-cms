fbf2d6d13a9db45ebc78bec29aff142f0a017fd9
STABY\staby <vsoghlv@naver.com>
2021-12-20 15:14:18 +0900

    graphql url 변경

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -32,7 +32,7 @@ export let isLoggedInVar: ReactiveVar<boolean> = makeVar(Boolean(null))
 export let authTokenVar: ReactiveVar<string> = makeVar('')
 
 const httpLink = createHttpLink({
-  uri: 'http://172.30.1.21:4000/graphql',
+  uri: 'http://cms-api-dev.staby.co.kr/graphql',
   credentials: 'same-origin',
 })
 
