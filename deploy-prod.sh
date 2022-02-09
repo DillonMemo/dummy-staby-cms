7d623bd24b3869bc5f438245f523fc35457c39d8
jason <jason@staby.co.kr>
2022-02-09 10:54:54 +0900

    [client] [jason] (fix) : 운영서버 배포 스크립트 오타 수정

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export PATH='/home/stabydev/.nvm/versions/node/v16.13.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PATH='/home/stabyprod/.nvm/versions/node/v16.13.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 
 cp ../.env.production .
 
