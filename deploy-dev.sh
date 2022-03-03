e36a2652d722ee5089fbba4d7bde4f4f3e46b5c1
dillonmemo <arta1069@gmail.com>
2022-03-03 10:28:54 +0900

    rollback deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -2,10 +2,8 @@
 
 export PATH='/home/stabydev/.nvm/versions/node/v16.13.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 
-cp ../.env.development ./.env
+cp ../.env.development .
 
 npm i
-
-npm run build
 
 pm2 reload staby-cms-client
